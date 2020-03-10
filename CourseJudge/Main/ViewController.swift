d89d9adc70d35fbf28a5f23e74dd3abba14fbea0
Michel Balamou <michelbalamou@gmail.com>
2020-03-09 23:18:55 -0400

    Initial commit

---
+++ CourseJudge/Main/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CourseJudge
+//
+//  Created by Michel Balamou on 2020-03-09.
+//  Copyright © 2020 Michel Balamou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
