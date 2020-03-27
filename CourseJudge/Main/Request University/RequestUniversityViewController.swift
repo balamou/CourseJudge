3f266279e278ea1465ca1dd2115349a8a8652e0c
Michel Balamou <michelbalamou@gmail.com>
2020-03-27 12:33:26 -0400

    Added request uni

--- CourseJudge/Main/Request University/RequestUniversityViewController.swift
+++ CourseJudge/Main/Request University/RequestUniversityViewController.swift
@@ -17,7 +17,30 @@ class RequestUniversityViewController: UIViewController {
         
         requestUniView = RequestUniversityView()
         view = requestUniView
+        
+        requestUniView.cancelButton.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
+        requestUniView.requestButton.addTarget(self, action: #selector(requestButtonTapped), for: .touchUpInside)
     }
+    
+    @objc func cancelButtonTapped() {
+        dismiss(animated: true, completion: {})
+    }
+    
+    @objc func requestButtonTapped() {
+        let universityName = requestUniView.universityName.textField.text!
+        let errorMessage = "cannot be empty"
+        
+        if universityName.isEmpty {
+              requestUniView.universityName.setError(message: errorMessage)
+        } else {
+            requestUniView.universityName.setNormal()
+           
+            requestUniView.showSuccess {
+                self.dismiss(animated: true, completion: {})
+            }
+        }
+    }
+    
     
 }
 
