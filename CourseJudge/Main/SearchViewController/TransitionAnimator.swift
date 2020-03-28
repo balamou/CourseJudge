439fc333c9f7937461912715e5f74109a75eb14d
Michel Balamou <michelbalamou@gmail.com>
2020-03-27 23:04:46 -0400

    Removed warning

--- CourseJudge/Main/SearchViewController/TransitionAnimator.swift
+++ CourseJudge/Main/SearchViewController/TransitionAnimator.swift
@@ -25,7 +25,7 @@ final class TransitionAnimator: NSObject, UIViewControllerAnimatedTransitioning 
         storedContext = transitionContext
         let containerView = transitionContext.containerView
        
-        guard let fromView = transitionContext.view(forKey: .from) else { return }
+//        guard let fromView = transitionContext.view(forKey: .from) else { return }
         guard let toView = transitionContext.view(forKey: .to) else { return }
         
         containerView.addSubview(toView)
